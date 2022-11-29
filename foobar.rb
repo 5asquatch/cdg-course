a8c190846d4442cf2d54fa7811fe6e9b4b536434
Sergey Savchkov <61937286+5asquatch@users.noreply.github.com>
2022-11-29 14:52:02 +0200

    Add files via upload

---
+++ foobar.rb
@@ -0,0 +1,16 @@
+def foobar (x,y)
+    if x == 20
+        result = y
+    elsif y == 20
+        result = x
+    else
+        result = x + y
+    end
+    print "Result = #{result}\n"
+end
+
+print "Enter first number: "
+x = gets.to_i
+print "Enter first number: "
+y = gets.to_i
+puts foobar(x,y)
---
+++ greetengs.rb
@@ -0,0 +1,13 @@
+def greetengs (name,age)
+    if age >= 18
+        print name + "cамое время заняться делом!"
+    else 
+        print name + "тебе меньше 18ти, но учиться программировать никогда не рано"
+    end
+end
+
+print "Enter your name: "
+name = gets.to_s
+print "Enter your age: "
+age = gets.to_i
+puts greetengs(name,age)
